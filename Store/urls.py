84825e446367f0ee3404515c5c8f2decc8539fa3
Raphadevs <raphaelcarvalho.law@gmail.com>
2022-08-10 22:20:15 -0300

    Envio de email

--- Store/urls.py
+++ Store/urls.py
@@ -1,7 +1,7 @@
-
 from django.urls import path
 from Store import views
-from Store.models import Produto
+
+
 
 urlpatterns = [
     path('', views.index, name= 'index'),
@@ -9,5 +9,8 @@ urlpatterns = [
     path('Departamentos/', views.Departamentos,name='departamentos'),
     path('categorias/<int:id>', views.categorias, name='categorias'),
     path('produtos/<int:id>', views.produtos, name= 'produtos'),
-    path('produto_detalhe/<int:id>', views.produtos,name='produto_detalhe')
+    path('produto_detalhe/<int:id>', views.produtos,name='produto_detalhe'),
+    path('institucional/', views.institucional, name='institucional'),
+    path('contato/', views.contato, name='contato'),
+    path('contato/enviar/', views.enviar_email, name= 'enviar_contato')
 ]
--- Store/views.py
+++ Store/views.py
@@ -1,8 +1,8 @@
 from ast import Return
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from Store.models import Departamento, Categoria, Produto
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -45,3 +45,31 @@ def produtos_detalhe(request, id):
                 'produto': produtos
     }
     return render (request, 'produto_detalhe.html',context)
+
+def institucional (request):
+    return render(request, 'institucional.html')
+
+def contato(request):
+    return render (request, 'contato.html')
+
+def enviar_email(request):
+    nome = request.POST['nome']
+    telefone = request.POST['telefone']
+    assunto = request.POST['assunto']
+    mensagem = request.POST['mensagem']
+    remetente = request.POST['email']
+    destinatario = ['raphaelcarvalho.law@gmail.com']
+    corpo = f"Nome: {nome} \nTelefone: {telefone}  \nMensagem : {mensagem}"
+    
+    try:
+        send_mail(assunto, corpo, remetente, destinatario)
+        context = {'msg': 'E-mail enviado com sucesso!'}
+        
+    except:
+        context = {'msg': 'Erro ao enviar e-mail!'}
+    return render(request, 'contato.html', context)
+
+
+
+
+
